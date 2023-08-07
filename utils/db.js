6467cbac777df4b332658b42c77c603bb2039462
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-07 16:27:08 +0200

    2

--- utils/db.js
+++ utils/db.js
@@ -1,52 +1,27 @@
-import { MongoClient } from 'mongodb'
-
-const DB_HOST = '127.0.0.1'
-const DB_PORT = '27017'
-const DB_DATABASE = 'files_manager';
-
-const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-
+import mongodb from 'mongodb';
 class DBClient {
   constructor() {
-    this.client = new MongoClient(url);
+    const DB_HOST = '127.0.0.1'
+    const DB_PORT = '27017'
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+    mongodb.MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      this.db = client.db(DB_DATABASE);
+    });
   }
 
+
   async isAlive() {
-    try {
-      await this.client.connect();
-      this.db = this.client.db();
-      return true
-    } catch (err) {
-      return false
-    }
+    return !!this.db;
   }
 
   async nbUsers() {
-    try {
-      await this.client.connect();
-      const usersCollection = this.db.collection('users');
-      const count = await usersCollection.countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error retrieving user count:', error);
-      throw error;
-    } finally {
-      this.client.close();
-    }
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    try {
-      await this.client.connect();
-      const filesCollection = this.db.collection('files');
-      const count = await filesCollection.countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error retrieving file count:', error);
-      throw error;
-    } finally {
-      this.client.close();
-    }
+    return this.db.collection('files').countDocuments();
   }
 }
 
