1376db285fc4579836e958db4bdf7a6f122cc732
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-14 13:09:49 +0200

    3

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || '127.0.0.1';
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
