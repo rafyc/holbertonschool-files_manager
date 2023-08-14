07bd9031cc04ba73a784f32dbadb8268cbfe2ea7
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-14 13:11:23 +0200

    3

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,32 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
+import redis from 'redis';
+import util from 'util';
 
-const router = express.Router();
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
 
-router.get('/status', AppController.getStatus());
-router.get('/stats', AppController.getStats());
+  isAlive() {
+    return this.client.connected;
+  }
 
-export default router;
+  async get(key) {
+    const getAsync = util.promisify(this.client.get).bind(this.client);
+    const reply = await getAsync(key);
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
