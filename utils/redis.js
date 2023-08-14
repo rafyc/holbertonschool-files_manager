230a3c5791d127d9f627f31ee0dbec447e0ef402
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-14 12:58:39 +0200

    3

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
