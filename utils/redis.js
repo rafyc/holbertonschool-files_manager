61b0f7ff4b93b64cacdaed93c4873a74ccec52b3
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-14 12:57:48 +0200

    3

--- utils/redis.js
+++ utils/redis.js
@@ -8,18 +8,5 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.get('/files/:id/data', FilesController.getFile);
-
-router.use(express.json());
-router.post('/users', UserController.postNew);
-router.post('/files', FilesController.postUpload);
-
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnPublish);
 
 export default router;
