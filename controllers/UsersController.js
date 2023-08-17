3945050d7b4ecdb4972744105cc3f2e2c4a204a3
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-17 17:59:45 +0200

    4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,19 +4,19 @@ import sha1 from 'sha1';
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    const user = dbClient.db.collection('users').findOne({ email });
+    const user = await dbClient.db.collection('users').findOne({ email });
     const hashedPassword = sha1(password);
 
     if (!email) {
-      return res.status(400).send({ error: 'Missing email' })
+      return res.status(400).send({ error: 'Missing email' });
     }
     if (!password) {
-      return res.status(400).send({ error: 'Missing password' })
+      return res.status(400).send({ error: 'Missing password' });
     }
     if (user) {
-      return res.status(400).send({ error: 'Already exist' })
+      console.log('lolo');
+      return res.status(400).send({ error: 'Already exist' });
     }
-
     const newUser = await dbClient.db.collection('users').insertOne({ email, pasword: hashedPassword });
     const id = newUser.insertedId;
     return res.status(201).send({ id, email });
