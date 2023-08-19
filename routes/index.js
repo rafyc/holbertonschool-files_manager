adce58c869a9f737900bd75c13dcbf353fb9b655
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-19 16:18:21 +0200

    5

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
@@ -31,6 +32,6 @@ router.post('/users', (request, response) => {
 
 router.post('/files', (request, response) => {
   FilesController.postUpload(request, response);
-});
+})
 
 export default router;
